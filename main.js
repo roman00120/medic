4f4b42154c913d88edf149ea9f90fa2414274092
Roman Velasco Moctezuma <78324706+roman00120@users.noreply.github.com>
2025-12-24 13:19:39 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+    const currentTheme = localStorage.getItem('theme');
+
+    // Revisar si ya había una preferencia guardada
+    if (currentTheme) {
+        document.documentElement.setAttribute('data-theme', currentTheme);
+        if (currentTheme === 'dark') {
+            toggleSwitch.checked = true;
+        }
+    }
+
+    // Función para cambiar el tema
+    function switchTheme(e) {
+        if (e.target.checked) {
+            document.documentElement.setAttribute('data-theme', 'dark');
+            localStorage.setItem('theme', 'dark');
+        } else {
+            document.documentElement.setAttribute('data-theme', 'light');
+            localStorage.setItem('theme', 'light');
+        }    
+    }
+
+    toggleSwitch.addEventListener('change', switchTheme, false);
+});
